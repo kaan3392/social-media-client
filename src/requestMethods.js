44d261132ed08d640e317bd515e761e9ccb5e611
kaan3392 <kaanalacali@gmail.com>
2022-04-23 04:38:57 +0300

    ..

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
-const BASE_URL = process.env.BASE_URL || "http://localhost:8800/api/";
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8800/api/"
+    : "https://social-media-knlcl.herokuapp.com/api";
 const TOKEN = JSON.parse(localStorage.getItem("user"))?.accessToken;
-
+console.log(BASE_URL);
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
