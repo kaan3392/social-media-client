742b66d6d59509348bffe8ff014d354adceef8bc
kaan3392 <kaanalacali@gmail.com>
2022-04-23 04:50:25 +0300

    ....

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -4,7 +4,7 @@ import {io} from "socket.io-client"
 const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "ws://localhost:8800"
-    : "ws://social-media-knlcl.herokuapp.com";
+    : "wss://social-media-knlcl.herokuapp.com";
 
 export const socket = io(BASE_URL, { transports: ['websocket'] });
 export const SocketContext = createContext();
